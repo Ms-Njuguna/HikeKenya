63b245b3f2f36a526db6195622cf7fa6ec8e738e
Patricia <patricianjuguna084@gmail.com>
2025-07-22 11:14:23 +0300

    Create MapPreview logic with route polyline to show route on map

--- HikeKenya/src/App.jsx
+++ HikeKenya/src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function App() {
 
+  const[trails, setTrails] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/trails")
+    .then(res => res.json())
+    .then((data) => {
+      setTrails(data)
+    })
+    .catch(err => console.error("Error fething the trails...", err))
+  }, []);
+  
   return (
     <div>
       <h1 className="text-4xl font-bold text-green-800 shadow-md text-center">Welcome to Hike Kenya</h1>
