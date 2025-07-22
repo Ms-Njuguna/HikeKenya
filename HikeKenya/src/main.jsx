1d1d365c8bdae63eed445979f12a141085be6e0f
Patricia <patricianjuguna084@gmail.com>
2025-07-22 20:05:03 +0300

    Add weather card logic and routing for different pages of the web app

--- HikeKenya/src/main.jsx
+++ HikeKenya/src/main.jsx
@@ -2,10 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'leaflet/dist/leaflet.css';
+import {AuthProvider} from './context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
