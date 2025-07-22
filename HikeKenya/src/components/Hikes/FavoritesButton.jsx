2877564ee501829a9d9ac8ec2dda8a51ef87a858
Newton Oduor <newtonochieng@gmail.com>
2025-07-22 16:11:56 +0300

    Adds PATCH request to update the new list of favorites

--- HikeKenya/src/components/Hikes/FavoritesButton.jsx
+++ HikeKenya/src/components/Hikes/FavoritesButton.jsx
@@ -16,12 +16,24 @@ useEffect (() => {
 
 //function to handle clicking 
 const handleToggleFavorite = () => {
-    fetch(`htpp://localhost:3000/users/${userId}`)
-    .then((res) => res.json)
+    fetch(`htpp://localhost:3000/users/${userId}`) //Fetch user data
+    .then((res) => res.json())
     .then((user) => {
         const updatefavorites = isFavorite ? user.favorites.filter((id) => id !== trailId)
-        : [...user.favorites, trailId];
+        : [...user.favorites, trailId]; //Update favorites
     })
+
+    fetch(`htpp://localhost:3000/users/${userId}`, {
+        method: 'PATCH',
+        headers: {
+            'content-type' : 'application/json',
+        },
+        body: JSON.stringify({ favorites : updatedFavorites }), 
+    })
+      .then((res) => res.json())
+      .then(() => {
+        setIsfavorite(!isFavorite);
+      }); 
 }
 
   return{
