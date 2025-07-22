9db519402f0b63c8abb8ff4415b83e55e13e797b
Bonnie <bonfasogaro@gmail.com>
2025-07-22 20:31:25 +0300

    adds Hike Card and hike Carousel logic

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-green-800 shadow-md text-center">Welcome to Hike Kenya</h1>
+    </div>
+  )
+}
+
+export default App
