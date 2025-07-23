e7e123d50de6c98a273df3cee8243110ae111227
Newton Oduor <newtonochieng@gmail.com>
2025-07-23 12:49:54 +0300

    Adds Function for users to join Hike

---
+++ HikeKenya/src/components/Hikes/JoinHikeButton.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState} from "react";
+
+function ({ userId, hikeId, onJoin }) {
+    
+    const [joined, setJoined] = useState(false);
+    
+    useEffect(() => {
+        fetch(`http://localhost:3000/users/${userId}`)
+        .then((res) => res.json())
+        .then((data) => setJoined(data.joinedHikes || []));
+    }, [userId]);
+
+    function handleJoinClick () {
+        if (joined.includes(hikeId)) return;
+
+        const updated = [...joined, hikeId];
+
+        fetch(`http://localhost:3000/users/${userId}`, {
+            method: "PATCH",
+            headers: {"Content-Type : application/json"},
+            body: JSON.stringify({ joinedHikes: updated}) 
+        })
+        .then((res) => res.json())
+        .then((data) => setJoined(data.joinedHikes));
+         onJoin && onJoin();
+        
+    }
+    
+    return (
+        <button onClick={handleJoinClick}>
+            {joined.includes(hikeId) ? "Joined ✅" : "Join Hike"}
+        </button>
+    );
+}
