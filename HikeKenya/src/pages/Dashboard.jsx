4ebb7221d1fc864310a76143897349a573e16546
Patricia <patricianjuguna084@gmail.com>
2025-07-23 12:47:21 +0300

    Add UserInfo logic using props to create user info card for the dashboard

--- HikeKenya/src/pages/Dashboard.jsx
+++ HikeKenya/src/pages/Dashboard.jsx
@@ -6,7 +6,11 @@ function Dashboard({ badges, users}) {
         <diV>
             <h1>This is the Dashboard page</h1>
             <div>
-                
+                {users.map((user) => {
+                    return(
+                        <UserInfo name={user.name} email={user.email} points={user.points}/>
+                    )
+                })}
             </div>
         </diV>
     );
