2b7508653d9e7a789acc41d8adb5a4273a5fea5d
Newton Oduor <newtonochieng@gmail.com>
2025-07-23 15:56:11 +0300

    Adds necessary comments

--- HikeKenya/src/components/Hikes/JoinHikeButton.jsx
+++ HikeKenya/src/components/Hikes/JoinHikeButton.jsx
@@ -1,20 +1,24 @@
 import React, { useEffect, useState} from "react";
 
+// Function that receives UserId, hikeId and onJoin
 function JoinHikeButton({ userId, hikeId, onJoin }) {
     
     const [joined, setJoined] = useState([]);
     
+    //Use effect to fetch user joined hikes
     useEffect(() => {
         fetch(`http://localhost:3000/users/${userId}`)
         .then((res) => res.json())
-        .then((data) => setJoined(data.joinedHikes || []));
+        .then((data) => setJoined(data.joinedHikes || [])); // Set joined hikes or fallback to an empty array
     }, [userId]);
-
+    
+    // Function to handle the join hike button
     function handleJoinClick () {
         if (joined.includes(hikeId)) return;
 
         const updated = [...joined, hikeId];
 
+        // Send PATCH request to update the users joined hikes (backend)
         fetch(`http://localhost:3000/users/${userId}`, {
             method: "PATCH",
             headers: {"Content-Type" : "application/json"},
@@ -22,14 +26,14 @@ function JoinHikeButton({ userId, hikeId, onJoin }) {
         })
         .then((res) => res.json())
         .then((data) => {setJoined(data.joinedHikes);
-         onJoin && onJoin();
+         onJoin && onJoin(); // call onJoin callback to refresh UI and update the list
         });
     }    
-    
+    // Return Onclick Button (The button is disabled if user already joined hike (line 36))
     return (
         <button onClick={handleJoinClick}
-        className={`px-3 py-1 rounded ${joined.includes(hikeId) ? "bg-gray-300" : "bg-green-500 text-white"}`}
-        disabled={joined.includes(hikeId)}>
+        className={`px-3 py-1 rounded ${joined.includes(hikeId) ? "bg-gray-300" : "bg-green-500 text-white"}`} // If joined show gray button, otherwise green
+        disabled={joined.includes(hikeId)}> 
             {joined.includes(hikeId) ? "Joined ✅" : "Join Hike"}
         </button>
     );
