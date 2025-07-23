8e530b5bc6312f1cfd4e22e158080293c992ba39
Patricia <patricianjuguna084@gmail.com>
2025-07-23 10:07:38 +0300

    Create states and fetch each piece of data neede in the App.jsx file

--- HikeKenya/src/App.jsx
+++ HikeKenya/src/App.jsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react'
 import AppRoutes from './routes/Routes.jsx';
 
-
-
 function App() {
 
   const[trails, setTrails] = useState([]);
+  const[users, setUsers] =useState([]);
+  const[badges, setBadges] = useState([]);
+  const[reviews, setReviews] = useState([]);
 
   useEffect(() => {
     const fetchTrails = async () => {
@@ -19,6 +20,48 @@ function App() {
     };
 
     fetchTrails();
+  }, []);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/users");
+        const data = await res.json();
+        setUsers(data);
+      } catch (err) {
+        console.error("Error fetching the users...", err);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const fetchBadges = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/badges");
+        const data = await res.json();
+        setBadges(data);
+      } catch (err) {
+        console.error("Error fetching the badges...", err);
+      }
+    };
+
+    fetchBadges();
+  }, []);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/reviews");
+        const data = await res.json();
+        setReviews(data);
+      } catch (err) {
+        console.error("Error fetching the reviews...", err);
+      }
+    };
+
+    fetchReviews();
   }, []);
   
   return (
