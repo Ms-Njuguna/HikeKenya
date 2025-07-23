b6964c543788d65b2f834af4849257820391ba51
Patricia <patricianjuguna084@gmail.com>
2025-07-23 14:16:28 +0300

    Fix App.jsx back to having fetch logic and passing props to the different routes

--- HikeKenya/src/App.jsx
+++ HikeKenya/src/App.jsx
@@ -1,12 +1,75 @@
-import React from "react";
-import LoginSignup from "./components/Auth/LoginSignup.jsx";
+import React, {useEffect, useState} from 'react'
+import AppRoutes from './routes/Routes.jsx';
 
 function App() {
+
+  const[trails, setTrails] = useState([]);
+  const[users, setUsers] =useState([]);
+  const[badges, setBadges] = useState([]);
+  const[reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchTrails = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/trails");
+        const data = await res.json();
+        setTrails(data);
+      } catch (err) {
+        console.error("Error fetching the trails...", err);
+      }
+    };
+
+    fetchTrails();
+  }, []);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/users");
+        const data = await res.json();
+        setUsers(data);
+      } catch (err) {
+        console.error("Error fetching the users...", err);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const fetchBadges = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/badges");
+        const data = await res.json();
+        setBadges(data);
+      } catch (err) {
+        console.error("Error fetching the badges...", err);
+      }
+    };
+
+    fetchBadges();
+  }, []);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/reviews");
+        const data = await res.json();
+        setReviews(data);
+      } catch (err) {
+        console.error("Error fetching the reviews...", err);
+      }
+    };
+
+    fetchReviews();
+  }, []);
+
   return (
     <div>
-      <LoginSignup />
+      <h1 className="text-4xl font-bold text-green-800 shadow-md text-center">Welcome to Hike Kenya</h1>
+      <AppRoutes trails={trails} users={users} badges={badges} reviews={reviews}/> 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
