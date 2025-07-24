e67ec199388625fdba4590e8fec7c0002256a64d
Newton Oduor <newtonochieng@gmail.com>
2025-07-24 06:00:56 +0300

    Adds fetch to update users payment history

--- HikeKenya/src/components/Mpesa/MpesaModal.jsx
+++ HikeKenya/src/components/Mpesa/MpesaModal.jsx
@@ -6,8 +6,33 @@ function MpesaModal({ trail, onClose }) {
     const [error, setError] = useState("")
     const [success, setSuccess] = useState(false)
 
+    // Function to validate phone number format (returns true if phone is valid ln 12)
     const isValidPhoneNumber = (phone) => {
         const phoneRegex = /^0\d{9}$/;
         return phoneRegex.test(phone);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventdefault();
+
+        if (!isValidPhoneNumber(phone)) {
+            setError("Enter a valid phone number (e.g 0722 123 456 or 0116 123 456)");
+            return;
+        }
     }
+
+    const userId = localStorage.getItem("userId");
+
+    // Fetch the users current data to update to update their payment history
+    fetch(`http://localhost:3000/users/${userId}`)
+    .then ((res) => res.json())
+    .then((user) => {
+        const newPayment = {
+            trail: trail.id,
+            title: trail.title,
+            amount: trail.price,
+            phone: phone,
+            date: new Date().toISOString(),
+        };
+    })
 }
